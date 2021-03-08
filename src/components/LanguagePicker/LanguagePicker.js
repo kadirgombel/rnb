f37491e2571fed413aad207a6e4e21ba6afc2ca5
Kadir Gömbel <l1115046@std.yildiz.edu.tr>
2021-03-08 03:11:53 +0300

    create lang picker

---
+++ src/components/LanguagePicker/LanguagePicker.js
@@ -0,0 +1,35 @@
+import { Picker } from 'native-base';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet } from 'react-native';
+
+export default function LanguagePicker() {
+  const { t, i18n } = useTranslation();
+  const langs = [
+    {
+      label: t('langPicker.turkish'),
+      value: 'tr',
+    },
+    {
+      label: t('langPicker.english'),
+      value: 'en',
+    },
+  ];
+  return (
+    <Picker
+      mode="dropdown"
+      style={styles.picker}
+      selectedValue={i18n.language}
+      onValueChange={(value) => i18n.changeLanguage(value)}>
+      {langs.map((lang) => (
+        <Picker.Item key={lang.value} label={lang.label} value={lang.value} />
+      ))}
+    </Picker>
+  );
+}
+
+const styles = StyleSheet.create({
+  picker: {
+    width: 120,
+  },
+});
